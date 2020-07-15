ed5b409c55c63cbbbca87105118c56bc553fe88c
AdMeynard <adrien9.meynard@laposte.net>
2020-07-15 19:04:06 -0400

    new results

---
+++ TimeFrequencyScaleRep/rsSTFT.m
@@ -0,0 +1,53 @@
+function [tfr, tfrtic, tfrrs, ConceFT, tfrrstic] = rsSTFT(x, lowFreq, highFreq, alpha, WinLen, dim, supp, MT) ;
+
+%
+% Usage: 
+% 	[tfrsq, ConceFT, tfrsqtic] = sqSTFT(t, x, lowFreq, highFreq, alpha, WinLen, dim, supp, MT)
+%
+% MT = 1: ordinary SST; MT > 1: ConceFT
+% alpha: resolution in the frequency axis
+% WinLen, dim, supp: for hermf.m
+%
+% Example:
+% 	[tfrsq, ConceFT, tfrsqtic] = sqSTFT([1:length(y)]', y, 0,0.5, 0.0002, 121, 4, 6, 10);
+
+
+N = length(x) ;
+
+%%%% Multitapering %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+       	%% generate the window for short time Fourier transform (STFT)
+[h, Dh, ~] = hermf(WinLen, dim, supp) ;
+
+
+%=======================================
+
+fprintf(['Run ordinary SST\n']) ;
+[tfr, tfrtic, tfrrs, tfrrstic] = rsSTFTbase(x, lowFreq, highFreq, alpha, 1, h(1,:)', Dh(1,:)', 1);
+
+
+%=======================================
+ConceFT = [] ;
+
+if MT > 1
+
+	%% Conceft
+    ConceFT = zeros(size(tfrrs)) ;
+
+	fprintf(['ConceFT total: ',num2str(MT),'; now:     ']) ;
+    for ii = 1: MT
+		fprintf('\b\b\b\b') ;	tmp = sprintf('%4d',ii) ; fprintf([tmp]) ;
+		rv = randn(1, dim) ; rv = rv ./ norm(rv) ;
+		rh = rv * h ; 
+		rDh = rv * Dh ;
+
+		[~, ~, tfrrs, tfrrstic] = rsSTFTbase(x, lowFreq, highFreq, alpha, 1, rh', rDh', 1);
+
+	 	ConceFT = ConceFT + tfrrs ;
+    end
+
+    ConceFT = ConceFT ./ MT ;
+	fprintf('\n') ;
+
+end
+
+end
---
+++ TimeFrequencyScaleRep/rsSTFTmultitaper.m
@@ -0,0 +1,43 @@
+function [MultiTaperSTFT, tfrtic, MultiTaper, MultiTaperAll, tfrrstic] = rsSTFTmultitaper(x, lowFreq, highFreq, alpha, WinLen, dim, supp) ;
+
+%
+% Usage: 
+% 	[tfrsq, ConceFT, tfrsqtic] = sqSTFTmultitaper(t, x, lowFreq, highFreq, alpha, WinLen, dim, supp)
+%
+% MT = 1: ordinary SST; MT > 1: ConceFT
+% alpha: resolution in the frequency axis
+% WinLen, dim, supp: for hermf.m
+%
+% Example:
+% 	[tfrsq, MultiTaper, tfrsqtic] = sqSTFT([1:length(y)]', y, 0,0.5, 0.0002, 121, 4, 6);
+
+
+N = length(x) ;
+
+%%%% Multitapering %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+       	%% generate the window for short time Fourier transform (STFT)
+[h, Dh, ~] = hermf(WinLen, dim, supp) ;
+
+
+%=======================================
+
+[tfr, tfrtic , tfrrs, tfrrstic] = rsSTFTbase(x, lowFreq, highFreq, alpha, 1, h(1,:)', Dh(1,:)', 1);
+
+MultiTaperAll = zeros(size(tfrrs,1), size(tfrrs,2), dim) ;
+MultiTaperAll(:,:,1) = tfrrs ;
+MultiTaperSTFT = tfr ;
+
+fprintf(['MultiTaper total: ',num2str(dim),'; now:     ']) ;
+for ii = 2: dim
+	fprintf('\b\b\b\b') ;	tmp = sprintf('%4d',ii) ; fprintf([tmp]) ;
+	[tfr, ~, tfrrs, tfrrstic] = rsSTFTbase(x, lowFreq, highFreq, alpha, 1, h(ii,:)', Dh(ii,:)', 1);
+
+ 	MultiTaperAll(:, :, ii) = tfrrs ;
+	MultiTaperSTFT = MultiTaperSTFT + tfr ;
+end
+
+MultiTaper = mean(MultiTaperAll, 3) ;
+MultiTaperSTFT = MultiTaperSTFT / dim ;
+fprintf('\n') ;
+
+end
